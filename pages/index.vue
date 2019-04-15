37f1407aa7d1c60513099f3638dfbf196e405f5a
Abhinav Jonnada <ajonnada@spillcenter.com>
2019-04-14 23:51:29 -0400

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-xs-center">
+        <logo/>
+        <vuetify-logo/>
+        <h1>Hello World</h1>
+        <li v-for="(product, key) in products" :key="key">
+          <ul>{{product}}</ul>
+        </li>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+import { db } from "../plugins/firebase";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+  data() {
+    return {
+      products: []
+    };
+  },
+  firestore() {
+    return {
+      products: db.collection("products")
+    };
+  }
+};
+</script>
+
---
+++ plugins/firebase.js
@@ -0,0 +1,24 @@
+import { VueFire } from "vuefire";
+import { Vue } from "vue";
+import firebase from "firebase/app";
+import "firebase/firestore";
+export { Vue };
+Vue.use("vuefire");
+
+export const config = {
+  apiKey: "AIzaSyBP8jPc6k5llX2xcj_8QNgZDfnDGDd6_xo",
+  authDomain: "ecommerce-91b8f.firebaseapp.com",
+  databaseURL: "https://ecommerce-91b8f.firebaseio.com",
+  projectId: "ecommerce-91b8f",
+  storageBucket: "ecommerce-91b8f.appspot.com",
+  messagingSenderId: "594958977262"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+  firebase.firestore();
+} else {
+  firebase.app();
+}
+
+export const db = firebase.firestore();
